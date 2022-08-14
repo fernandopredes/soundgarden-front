28e93f651e2e43dc64d2bab14999572b61fb7334
fernando predes <fernando_predes@hotmail.com>
2022-08-14 20:06:48 -0300

    criado reservas.html

---
+++ js/listarReservas.js
@@ -0,0 +1,32 @@
+
+
+const listarReservas = async() => {
+  const respostaAPI = await fetch('https://xp41-soundgarden-api.herokuapp.com/bookings')
+  if (respostaAPI.status === 200) {
+    const dado = await respostaAPI.json()
+    return dado
+  }
+}
+
+const renderizarReservas = async(reservas) => {
+
+  const dado = await listarReservas(reservas)
+
+  dado.forEach((reserva, index) => {
+    const tabela = document.querySelector('tbody');
+    const novaLinha = document.createElement('tr');
+
+    novaLinha.innerHTML = `<th scope="row">${index + 1}</th>
+    <td>${reserva.owner_name}</td>
+    <td>${reserva.owner_email}</td>
+    <td>${reserva.number_tickets}</td>
+        <td>
+
+        </td >` //colocar as ações das reservas aqui
+
+    tabela.appendChild(novaLinha)
+  });
+
+
+}
+renderizarReservas()
