864feeab38aaadf2a5d205215c18e15c76f962d6
marcos-px <marcxstx@gmail.com>
2022-08-16 21:31:00 -0300

    finaliza index e eventos

--- js/index.js
+++ js/index.js
@@ -62,6 +62,8 @@ function direcionaEventos(eventos){
         let modal = document.querySelector('#modal-1');
           modal.style.display = 'none';
     }
+
+const modalReserva = document.querySelector('#modal-1');   
 // Acessa a array e seleciona quais ítens quer carregar
 async function main() {
     try {
@@ -74,10 +76,77 @@ async function main() {
         setEventListenerOnModalButton();
     } catch (error) {
     }
+}
 
+// seleciona os dados do modal para enviar a reserva.
+async function reservarIngressoModal() {
+  let inputNome = document.getElementById('nome').value
+  let inputEmail = document.getElementById('email').value
+  let inputLotacao = document.getElementById('quantidade').value
+  let inputID = document.getElementById('event-id').value
+
+  const postContent = {
+    "owner_name": inputNome,
+    "owner_email": inputEmail,
+    "number_tickets": inputLotacao,
+    "event_id": inputID
+
+  }
+
+  const fetchResultado = await fetch("https://xp41-soundgarden-api.herokuapp.com/bookings", {
+    method: 'POST',
+    body: JSON.stringify(postContent),
+    headers: {
+        "Content-type": "application/json"
+    }
+    })
+    .then(function (response) {
+      if (response.status == 201){
+        window.alert("Ingresso reservado, curta o show!")
+        return console.log(response.json())
+      }
+      else
+        window.alert("Deu ruim, tente novamente por favor!")
+
+})
+  .catch(function (err) {
+      return console.log(err.message)
+  })
+  reservarIngressoModal();
+}
+
+/* */
+async function verReservas(self){
+    let id = self.dataset.event_id
+    let todasReservas = self.dataset.todasReservas
+    const fetchResutlado = await fetch(`https://xp41-soundgarden-api.herokuapp.com/bookings/event/${id}`, {
+      method: 'GET',
+      headers: {
+          "Content-type": "application/json"
+      }
+  })
+  .then(function (response) {
+      return response.json()
+  })
+  .then(function (json) {
+      var reservasEfetuadas = 0
+
+      for (var i = 0; i < json.length; i++) {
+          reservasEfetuadas += Number(json[i].number_tickets)
+      }
+      document.getElementById('reservasEfetuadas').value = reservasEfetuadas
+      document.getElementById('reservasRestantes').value = reservasTotais
+      document.getElementById('event-id').value = id
+  })
+  verReservas();
 
 }
 
-
+function divIndexOculta(e) { 
+  if (e.matches)
+      document.getElementsByClassName('col')[1].style.display = "none"
+  else 
+      document.getElementsByClassName('col')[1].style.display = "block"
+}
 
 main();
